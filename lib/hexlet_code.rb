dadce7421ddf29b39d5dd30bd7dc8710fbfd36eb
Andrey Anisimov <maddbuzz@gmail.com>
2023-01-14 20:55:58 +0800

    refactor

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -8,61 +8,59 @@ module HexletCode
   module Tag
     def self.build(tag_name, attributes = {}, &block)
       attr_str = attributes.map { |k, v| " #{k}=\"#{v}\"" }.join
-      if block_given?
-        body = block.call
-        "<#{tag_name}#{attr_str}>#{body}</#{tag_name}>"
-      else
-        "<#{tag_name}#{attr_str}>"
-      end
+      return "<#{tag_name}#{attr_str}>" unless block_given?
+
+      content = block.call
+      body =
+        if content.instance_of?(Array)
+          content
+            .map do |child|
+              child_body = child[:body]
+              child_block = child_body && proc { child_body }
+              build(child[:tag_name], child[:attributes], &child_block)
+            end
+            .join
+        else
+          content
+        end
+      "<#{tag_name}#{attr_str}>#{body}</#{tag_name}>"
     end
   end
 
   class Form
-    def initialize(user, url, method, **kwargs)
-      @user = user
-      @url = url
-      @method = method
-      @other_attributes = kwargs
-      @children = []
+    attr_reader :model
+
+    def initialize(entity, attributes:)
+      @entity = entity
+      @model = { tag_name: 'form', attributes:, body: [] }
+    end
+
+    def submit(value = 'Save')
+      @model[:body] << { tag_name: 'input', attributes: { type: 'submit', value: }, body: nil }
     end
 
     def input(name, as: nil, **kwargs)
-      @children << { tag_name: 'label', attributes: { for: name }, body: name.capitalize }
+      @model[:body] << { tag_name: 'label', attributes: { for: name }, body: name.capitalize }
       return textarea(name, **kwargs) if as == :text
 
-      value = @user.public_send(name)
-      @children << { tag_name: 'input', attributes: { name:, type: 'text', value:, **kwargs }, body: nil }
+      value = @entity.public_send(name)
+      @model[:body] << { tag_name: 'input', attributes: { name:, type: 'text', value:, **kwargs }, body: nil }
     end
+
+    private
 
     def textarea(name, **kwargs)
       default_params = { cols: '20', rows: '40' }
       params = default_params.merge(kwargs)
-      value = @user.public_send(name)
-      @children << { tag_name: 'textarea', attributes: { name:, **params }, body: value }
-    end
-
-    def submit(value = 'Save')
-      @children << { tag_name: 'input', attributes: { type: 'submit', value: }, body: nil }
-    end
-
-    def build_template(template_builder)
-      # template_builder.call(tag_name, attributes, &body)
-      template_builder.call('form', action: @url, method: @method, **@other_attributes) do
-        @children
-          .map do |child|
-            body = child[:body]
-            block = body && proc { body }
-            template_builder.call(child[:tag_name], child[:attributes], &block)
-          end
-          .join
-      end
+      value = @entity.public_send(name)
+      @model[:body] << { tag_name: 'textarea', attributes: { name:, **params }, body: value }
     end
   end
 
-  def self.form_for(user, url: '#', method: 'post', **kwargs)
-    form = Form.new user, url, method, **kwargs
+  def self.form_for(entity, url: '#', method: 'post', **kwargs)
+    form = Form.new entity, attributes: { action: url, method:, **kwargs }
     yield form
-    template_builder = ->(tag_name, attributes, &body) { Tag.build(tag_name, attributes, &body) }
-    form.build_template template_builder
+    model = form.model
+    Tag.build(model[:tag_name], model[:attributes]) { model[:body] }
   end
 end
